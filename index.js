ad975e70a175633828a391fb1d9b4306069cbcf3
picantitoDev <155034777+picantitoDev@users.noreply.github.com>
2025-03-25 22:19:33 -0500

    Added handler for basic routes

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express")
 const app = express()
 
+// Import router
+const newMessageRoute = require("./routes/newMessageRoute")
+
 app.get("/", (req, res) => {
   res.send("Testing...")
 })
+
+app.use("/new", newMessageRoute)
 
 app.listen(8080, () => {
   console.log("Listening on port 8080...")
