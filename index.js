e35a13831b928ba254b367393f12c4c33da781db
picantitoDev <155034777+picantitoDev@users.noreply.github.com>
2025-03-25 22:42:00 -0500

    Step 4 completed, rendering messages

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const app = express()
 
 // Import router
 const newMessageRoute = require("./routes/newMessageRoute")
+const indexRoute = require("./routes/indexRoute")
 
-app.get("/", (req, res) => {
-  res.send("Testing...")
-})
+// Set up EJS
+const path = require("path")
+const layouts = require("express-ejs-layouts")
+app.use(layouts)
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
 
+app.use("/", indexRoute)
 app.use("/new", newMessageRoute)
 
 app.listen(8080, () => {
---
+++ routes/indexRoute.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const router = express.Router()
+
+// Messages
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+]
+
+router.get("/", (req, res) => {
+  res.render("index", { messages: messages })
+})
+
+module.exports = router
